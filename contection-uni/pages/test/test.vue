e37b105fe9d3a43f1174d58f5c44c2cc151b298e
FanGaoXS <954278478@qq.com>
2021-08-27 18:57:07 +0800

    contection-uni@wqk 经典蓝牙连接测试

--- contection-uni/pages/test/test.vue
+++ contection-uni/pages/test/test.vue
@@ -1,17 +1,30 @@
 <template>
 	<view>
-		<button @tap="buttonOpenBluetoothAdapter()">打开蓝牙并开始搜索附近蓝牙</button>
-		<button @tap="stopBluetoothDevicesDiscovery()">关闭蓝牙</button>
+		<button @tap="buttonOpenBluetoothAdapter()">开始搜索附近设备</button>
+		<button @tap="buttonGetBluetoothDevices()">获取已发现或者已连接的蓝牙设备</button>
+		<button @tap="stopBluetoothDevicesDiscovery()">停止搜索</button>
 		<view v-for="item in devicesList" :key="item.id" style="margin-top: 20rpx;">
-			蓝牙设备id：{{item.deviceId}}
-			蓝牙信号强度：{{item.RSSI}}
-			蓝牙设备名称：{{item.name}}
+			<view>蓝牙设备id：{{item.deviceId}}</view>
+			<view>蓝牙信号强度：{{item.RSSI}}</view>
+			<view>蓝牙设备名称：{{item.name}}</view>
+			<view>advertisData:{{item.advertisData | ab2hex}}</view>
+			<view>advertisServiceUUIDs:{{item.advertisServiceUUIDs}}</view>
+			<view>localName:{{item.localName}}</view>
+			<view>serviceData:{{item.serviceData}}</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	
+	import {
+		ab2hex
+		} from './filters.js'
+	
 	export default {
+		filters: {
+			ab2hex
+		},
 		data() {
 			return {
 				title: '测试蓝牙',
@@ -37,6 +50,10 @@
 				  },
 					fail:(error)=>{		//本机蓝牙未打开
 						console.log('openBluetoothAdapter fail->',error)
+						uni.showToast({
+							title: '蓝牙适配器打开失败，请检查本机蓝牙是否打开',
+							position: 'center'
+						})
 					}
 				})
 			},
@@ -60,7 +77,13 @@
 				uni.onBluetoothDeviceFound(res=>{
 					// console.log(res);	//返回新搜索到的设备列表 Array<Object>
 					const device = res.devices[0];	//搜到的设备
-					console.log(device);
+					console.log("device: ",device);
+					/*console.log('name->',device.name);
+					console.log('deviceId->',device.deviceId);
+					console.log('RSSI->',device.RSSI);
+					console.log('advertisServiceUUIDs->',device.advertisServiceUUIDs);
+					console.log('localName->',device.localName);
+					console.log('serviceData->',device.serviceData);*/
 					if(this.devicesList.indexOf(device.deviceId)==-1){	//已搜到设备列表中不包含新搜到的蓝牙设备则加入
 						this.devicesList.push(device)
 					}
@@ -79,7 +102,18 @@
 						this.devicesList = []
 					}
 				})
-				
+			},
+			// 获取在蓝牙模块生效期间所有已发现的蓝牙设备。包括已经和本机处于连接状态的设备。
+			buttonGetBluetoothDevices(){
+				uni.getBluetoothDevices({
+					success: res=>{
+						console.log('getBluetoothDevices success->',res);
+						console.log(res.devices);
+					},
+					fail: error=>{
+						console.log('getBluetoothDevices fail->',error);
+					}
+				})
 			}
 		}
 	}
